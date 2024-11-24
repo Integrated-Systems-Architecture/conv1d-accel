514aead9d84953f0db67bcd8d7d62992b48be65e
Luigi Giuffrida <luigi.giuffrida@polito.it>
2024-11-24 19:17:06 +0100

    Add hw templates for conv1d accelerator

---
+++ hw/conv1d.sv
@@ -0,0 +1,27 @@
+// Copyright 2024 Politecnico di Torino.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 2.0 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-2.0. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// File: cnt.sv
+// Author(s):
+//   Luigi Giuffrida
+//   Michele Caon
+// Date: 08/11/2024
+// Description: conv1d accelerator top module
+
+module conv1d (
+  /* verilator lint_off UNUSED */  // TODO: Remove this line when the signal is used
+  input logic clk_i,
+  input logic rst_ni
+
+);
+
+  // TODO: write here your code, you are encouraged to use a hierarchical (but not too hierarchical) approach.
+
+endmodule
